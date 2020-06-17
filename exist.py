bd0c36e006fcbdbed57469b856826073aa59d304
IgorMakhinich <igor.makhinich@gmail.com>
2020-06-17 10:31:37 +0300

    Add files via upload
    Parse products using asyncio(As site renders using JS, need to parse it asynchronously.), pyppeteer, BeautifulSoup.
    To parse items add url(line 7) and file name(line 8).
    Parser will create csv file with 'name', 'code', 'price' columns.

---
+++ exist.py
@@ -0,0 +1,63 @@
+import asyncio
+from pyppeteer import launch
+from bs4 import BeautifulSoup
+import csv
+
+URL = 'https://exist.ua/akkumuljatornye-batarei/trademark=eurostart/?page={}'
+HOST = 'https://exist.ua//'
+GOODS = []
+FILE_NAME = 'Eurostart'
+FILE = 'exist' + FILE_NAME + '.csv'
+
+async def main():
+    browser = await launch()
+    page = await browser.newPage()
+    # Поиск количества страниц в пагинации
+    pages_count = await get_pages_count(browser,page)
+    await browser.close()
+    # Получение данных со всех страниц
+    for page in range(1, pages_count + 1):
+        print(f'Обработка страницы {page} из {pages_count}...')
+        await parse_item(URL.format(page))
+        print(f'Получено {len(GOODS)} товаров')
+    # Сохранение данных в файл
+    save_file(GOODS, FILE)
+
+async def get_pages_count(browser, page):
+    await page.goto(URL.format(1))
+    await page.waitForSelector('footer.container-fluid')
+    pages_count = await page.querySelectorAllEval('div.pager', '(elements => elements.map(e => e.outerHTML))')
+    if len(pages_count) > 1:
+        count = BeautifulSoup(pages_count[1], 'html.parser').findAll('li')
+        return int(len(count)-1)
+    else:
+        return 1
+
+async def parse_item(url):
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto(url, timeout=1000000)
+    await page.waitForSelector('.catalogue-list', timeout=1000000)
+    offers = await page.querySelectorEval('.catalogue-list', '(element) => element.outerHTML')
+    soup = BeautifulSoup(offers, 'html.parser')
+    items = soup.find_all('div', class_='info-block')
+    for item in items:
+        try:
+            price = int(item.find('div', class_='price').get_text().replace(' грн', ''))
+        except:
+            price = False
+        if price:
+            GOODS.append({
+                'title': str(item.find('div', class_='title-wrapper').find_next('strong').get_text()),
+                'code': str(item.find('div', class_='trademark').get_text()),
+                'price': int(item.find('div', class_='price').get_text().replace(' грн', ''))
+            })
+
+def save_file(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['Название', 'Код', 'Цена'])
+        for item in items:
+            writer.writerow([item['title'], item['code'], item['price']])
+
+df = asyncio.get_event_loop().run_until_complete(main())
